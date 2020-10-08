c9591905db6817ce4b884ad25601c28f0317b717
burykin-n <bampane2008@yandex.ru>
2020-10-08 10:27:23 +0300

    Test

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,22 +7,20 @@ public class Main {
 
         public static int Min(int a,int b,int c)
         {
-            if (a>b && a>c) {
-                if (b>c)
+            if (a<=b && a<=c)
+                return a;
+            else if (b>c)
                     return c;
                 else
                     return b;
-            }
-            else if (a<b && a<c)
-                return a;
-            else if (b>c)
-                return c;
-            else
-                return b;
         }
+
+
     public static void main(String[] args) {
         System.out.print("Введите 3 целых числа через пробел: ");
         Scanner input = new Scanner(System.in);
+
+        
         //Проверку на тип вводимых данных не сделал
         System.out.print("Наимееньше из введеных чисел это "+Min(input.nextInt(),input.nextInt(),input.nextInt()));
     }
